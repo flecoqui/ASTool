68b8d1f2108822c4b4b1f7e3e5ba92eabee19426
Frédéric Le Coquil <flecoqui@microsoft.com>
2018-05-16 12:12:46 +0200

    fix issue with windows installation

--- Azure/101-vm-astool-universal/install-software-windows.ps1
+++ Azure/101-vm-astool-universal/install-software-windows.ps1
@@ -210,13 +210,13 @@ Start-Process -FilePath "C:\Program Files\Git\bin\git.exe" -Wait -ArgumentList "
 
 cd ASTool\cs\ASTool\ASTool
 Start-Process -FilePath "$env:USERPROFILE\AppData\Local\Microsoft\dotnet\dotnet.exe" -Wait -ArgumentList  "publish", "--self-contained", "-c", "Release", "-r", "win10-x64"
-cd bin\Release\netcoreapp2.0\ubuntu.16.10-x64\publish
+cd bin\Release\netcoreapp2.0\win10-x64\publish
 astool --help
-Start-Process -FilePath "c:\git\ASTool\cs\ASTool\ASTool\bin\Release\netcoreapp2.0\ubuntu.16.10-x64\publish\ASTool.exe" -Wait -ArgumentList "--help"
+Start-Process -FilePath "c:\git\ASTool\cs\ASTool\ASTool\bin\Release\netcoreapp2.0\win10-x64\publish\ASTool.exe" -Wait -ArgumentList "--help"
 WriteLog "ASTOOL built" 
 
 WriteLog "Installing ASTOOL as a service" 
-sc.exe create ASTOOL binpath= "cmd.exe /c c:\git\ASTool\cs\ASTool\ASTool\bin\Release\netcoreapp2.0\ubuntu.16.10-x64\publish\ASTool.exe \git\config\astool.windows.xml" type= own start= auto DisplayName= "ASTOOL"
+sc.exe create ASTOOL binpath= "cmd.exe /c c:\git\ASTool\cs\ASTool\ASTool\bin\Release\netcoreapp2.0\win10-x64\publish\ASTool.exe \git\config\astool.windows.xml" type= own start= auto DisplayName= "ASTOOL"
 WriteLog "ASTOOL Installed" 
 
 WriteLog "Initialization completed !" 
