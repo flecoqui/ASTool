80d20ac7ecf416b24d07b21bc39986b87a04bba6
Nicolas CLERC <nicolas.clerc@sablefin.net>
2018-05-16 13:54:40 +0200

    ASTool stateless service. 1st test ok.

---
+++ cs/ASTool/AdaptativeStreamingService/OptionsLauncher.cs
@@ -0,0 +1,45 @@
+﻿using ASTool;
+using ASTool_cli;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ASTool_cli
+{
+    public class OptionsLauncher
+    {
+        public delegate bool OptionsThread(Options opt);
+        public Options Option;
+        public OptionsThread Proc;
+
+        public OptionsLauncher(OptionsThread t, Options opt)
+        {
+            Proc = t;
+            Option = opt;
+        }
+        public void ThreadEntry()
+        {
+            if ((Proc != null) && (Option != null))
+            {
+                Proc(Option);
+            }
+        }
+        public static bool LaunchThread(OptionsThread ot, Options o)
+        {
+            bool result = false;
+            OptionsLauncher ol = new OptionsLauncher(ot, o);
+            System.Threading.ThreadStart threadStart = new System.Threading.ThreadStart(ol.ThreadEntry);
+
+            System.Threading.Thread t = new System.Threading.Thread(new System.Threading.ThreadStart(ol.ThreadEntry));
+            if (t != null)
+            {
+                t.Start();
+                result = true;
+            }
+
+            return result;
+
+        }
+
+    }
+}
